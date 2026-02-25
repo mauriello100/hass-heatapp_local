560f7e817ac663d8a8191e69b3b8a7f17beaca3b
mauriello100 <154350879+mauriello100@users.noreply.github.com>
2026-02-25 15:49:14 +0000

    Update __init__.py

--- custom_components/heatapp_local/__init__.py
+++ custom_components/heatapp_local/__init__.py
@@ -6,30 +6,24 @@ from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN, CONF_HOST, CONF_USER, CONF_PASSWORD, CONF_INTERVAL
-from .coordinator import heatAppDeviceUpdateCoordinator as Coordinator
 
 PLATFORMS: list[Platform] = [Platform.CLIMATE]
 
 
 async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     """Set up heatapp from a config entry."""
+    from .coordinator import heatAppDeviceUpdateCoordinator as Coordinator
 
     hass.data.setdefault(DOMAIN, {})
-    # TODO 1. Create API instance
-    # TODO 2. Validate the API connection (and authentication)
-    # TODO 3. Store an API object for your platforms to access
-    # hass.data[DOMAIN][entry.entry_id] = MyApi(...)
 
-    coordinator = Coordinator(hass, config_entry.options[CONF_HOST], config_entry.options[CONF_USER], 
-                                       config_entry.options[CONF_PASSWORD], config_entry.options[CONF_INTERVAL])
-    # try:
-    #     await coordinator.async_config_entry_first_refresh()
-    # except ConfigEntryNotReady:
-    #     await coordinator.api.close()
-    #     raise
+    coordinator = Coordinator(
+        hass,
+        config_entry.data[CONF_HOST],
+        config_entry.data[CONF_USER],
+        config_entry.data[CONF_PASSWORD],
+        config_entry.options.get(CONF_INTERVAL, 30),
+    )
 
-    # Finalize
-    hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][config_entry.entry_id] = coordinator
 
     await coordinator.async_config_entry_first_refresh()
@@ -46,6 +40,7 @@ async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> 
 
     return unload_ok
 
+
 async def update_listener(hass: HomeAssistant, config_entry: ConfigEntry) -> None:
     """Update listener."""
     await hass.config_entries.async_reload(config_entry.entry_id)
@@ -54,40 +49,40 @@ async def update_listener(hass: HomeAssistant, config_entry: ConfigEntry) -> Non
 ################################
 # """The heatapp_local integration."""
 # import asyncio
-
+#
 # import voluptuous as vol
-
+#
 # from homeassistant.config_entries import ConfigEntry
 # from homeassistant.core import HomeAssistant
-
+#
 # from .const import DOMAIN
-
+#
 # CONFIG_SCHEMA = vol.Schema({DOMAIN: vol.Schema({})}, extra=vol.ALLOW_EXTRA)
-
+#
 # # TODO - DONE List the platforms that you want to support.
 # # For your initial PR, limit it to 1 platform.
 # PLATFORMS = ["climate"]
-
-
+#
+#
 # async def async_setup(hass: HomeAssistant, config: dict):
 #     """Set up the heatapp_local component."""
 #     return True
-
-
+#
+#
 # async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 #     """Set up heatapp_local from a config entry."""
 #     # TODO Store an API object for your platforms to access
 #     # hass.data[DOMAIN][entry.entry_id] = MyApi(...)
 #     #hass.data[DOMAIN][entry.entry_id] = 
-
+#
 #     for component in PLATFORMS:
 #         hass.async_create_task(
 #             hass.config_entries.async_forward_entry_setup(entry, component)
 #         )
-
+#
 #     return True
-
-
+#
+#
 # async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
 #     """Unload a config entry."""
 #     unload_ok = all(
@@ -100,5 +95,5 @@ async def update_listener(hass: HomeAssistant, config_entry: ConfigEntry) -> Non
 #     )
 #     if unload_ok:
 #         hass.data[DOMAIN].pop(entry.entry_id)
-
+#
 #     return unload_ok
