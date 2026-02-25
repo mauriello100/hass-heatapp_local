2703f5ee3c3534c576e6591363ba7e26aeb9f8df
mauriello100 <154350879+mauriello100@users.noreply.github.com>
2026-02-25 19:07:13 +0000

    Update coordinator.py

--- custom_components/heatapp_local/coordinator.py
+++ custom_components/heatapp_local/coordinator.py
@@ -9,9 +9,9 @@ from collections import OrderedDict
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
 
-from pymodbus.payload import BinaryPayloadDecoder 
-from pymodbus.constants import Endian
-from pymodbus.register_read_message import ReadHoldingRegistersResponse
+# from pymodbus.payload import BinaryPayloadDecoder 
+# from pymodbus.constants import Endian
+# from pymodbus.register_read_message import ReadHoldingRegistersResponse
 
 from datetime import timedelta
 
