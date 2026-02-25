9475330f7d157d359c21d92eead3291412da9822
mauriello100 <154350879+mauriello100@users.noreply.github.com>
2026-02-25 19:12:20 +0000

    Update __init__.py

--- custom_components/heatapp_local/__init__.py
+++ custom_components/heatapp_local/__init__.py
@@ -1,34 +1,50 @@
 """The heatapp_local integration."""
-from _future import annotations
+from __future__ import annotations
 
-from homeassistant.config_entries import ConfigEntry from homeassistant.const import Platform from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN, CONF_HOST, CONF_USER, CONF_PASSWORD, CONF_INTERVAL
 
 PLATFORMS: list[Platform] = [Platform.CLIMATE]
 
-async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool: """Set up heatapp from a config entry.""" from .coordinator import heatAppDeviceUpdateCoordinator as Coordinator
 
-hass.data.setdefault(DOMAIN, {})
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+    """Set up heatapp from a config entry."""
+    from .coordinator import heatAppDeviceUpdateCoordinator as Coordinator
 
-coordinator = Coordinator(
-    hass,
-    config_entry.data[CONF_HOST],
-    config_entry.data[CONF_USER],
-    config_entry.data[CONF_PASSWORD],
-    config_entry.options.get(CONF_INTERVAL, 30),
-)
+    hass.data.setdefault(DOMAIN, {})
 
-hass.data[DOMAIN][config_entry.entry_id] = coordinator
+    coordinator = Coordinator(
+        hass,
+        config_entry.data[CONF_HOST],
+        config_entry.data[CONF_USER],
+        config_entry.data[CONF_PASSWORD],
+        config_entry.options.get(CONF_INTERVAL, 30),
+    )
 
-await coordinator.async_config_entry_first_refresh()
-config_entry.async_on_unload(config_entry.add_update_listener(update_listener))
-await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
+    hass.data[DOMAIN][config_entry.entry_id] = coordinator
 
-return True
-async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool: """Unload a config entry.""" if unload_ok := await hass.config_entries.async_unload_platforms(config_entry, PLATFORMS): hass.data[DOMAIN].pop(config_entry.entry_id) return unload_ok
+    await coordinator.async_config_entry_first_refresh()
+    config_entry.async_on_unload(config_entry.add_update_listener(update_listener))
+    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
 
-async def update_listener(hass: HomeAssistant, config_entry: ConfigEntry) -> None: """Handle options updates by reloading the entry.""" await hass.config_entries.async_reload(config_entry.entry_id)
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    if unload_ok := await hass.config_entries.async_unload_platforms(config_entry, PLATFORMS):
+        hass.data[DOMAIN].pop(config_entry.entry_id)
+
+    return unload_ok
+
+
+async def update_listener(hass: HomeAssistant, config_entry: ConfigEntry) -> None:
+    """Update listener."""
+    await hass.config_entries.async_reload(config_entry.entry_id)
+
 
 ################################
 # """The heatapp_local integration."""
